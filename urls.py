9f60f33406e91b5332c2769996defae5b031cd2d
sinabahm2oo6 <sinabahmanpoor77@gmail.com>
2026-06-24 19:02:03 +0330

    Update urls.py to add complete_profile route

--- urls.py
+++ urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('complete-profile/', views.CompleteProfileView.as_view(), name='complete_profile'),
     path('home/', views.HomeView.as_view(), name='home'),
-    path('home/', views.StudentHomeView.as_view(), name='student_home'),
-    path('home/', views.AdminHomeView.as_view(), name='admin_home'),
-    path('home/', views.AdvisorHomeView.as_view(), name='advisor_home')
+    path('student-home/', views.StudentHomeView.as_view(), name='student_home'),
+    path('admin-home/', views.AdminHomeView.as_view(), name='admin_home'),
+    path('advisor-home/', views.AdvisorHomeView.as_view(), name='advisor_home')
 ]
